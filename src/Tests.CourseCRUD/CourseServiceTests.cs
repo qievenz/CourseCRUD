1d3b9572c28ace1a5ade6ea98fb4fdc2209f990a
Ivan <ivann96@gmail.com>
2024-09-30 19:31:31 -0300

    Tests

---
+++ src/Tests.CourseCRUD/CourseServiceTests.cs
@@ -0,0 +1,16 @@
+namespace Tests.CourseCRUD
+{
+    public class CourseServiceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
