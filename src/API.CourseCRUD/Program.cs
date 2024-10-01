5343c8930116fef1ba51464dfc846bb4cf8f9620
Ivan <ivann96@gmail.com>
2024-10-01 13:07:44 -0300

    fix swagger release

--- src/API.CourseCRUD/Program.cs
+++ src/API.CourseCRUD/Program.cs
@@ -36,26 +36,17 @@ using (var scope = app.Services.CreateScope())
     var services = scope.ServiceProvider;
     var context = services.GetRequiredService<ApplicationDbContext>();
 
-    await Task.Delay(10000);
+    await Task.Delay(20000);
     context.Database.Migrate();
 }
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.MapFallbackToFile("/index.html");
 
 app.Run();
